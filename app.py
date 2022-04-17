7511ccda02996677280d04a0a46f4ed94df1737b
Sbastien Manic <sebastien.manic@gmail.com>
2022-04-17 11:19:17 +0200

    changement structure pour utiliser dossier resources

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
-from flask_restful import Resource, Api
-
+from flask_restful import Api
+from resources.helloworld import HelloWorld
 app = Flask(__name__)
 api = Api(app)
-
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
 
