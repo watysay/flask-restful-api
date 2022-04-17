9c6de3f40174041a40c2e3df1d844c6757b81e88
Sbastien Manic <sebastien.manic@gmail.com>
2022-04-17 10:34:09 +0200

    switch to flask restful

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
